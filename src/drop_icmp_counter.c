0e85efbd6c486e8eb10fba795969db813df0e203
xeome <xeome@proton.me>
2022-09-02 16:05:10 +0300

    add documentation to icmp counter

--- src/drop_icmp_counter.c
+++ src/drop_icmp_counter.c
@@ -11,7 +11,7 @@
 
 #define OVER(x, d) (x + 1 > (typeof(x))d)
 
-
+/* Creating a BPF map for counting ICMP packets as described above */
 struct bpf_map_def SEC("maps") cnt = {
     .type = BPF_MAP_TYPE_HASH,
     .key_size = sizeof(__be32),
@@ -36,6 +36,9 @@ static inline void csum_replace2(uint16_t *sum, uint16_t old, uint16_t new)
 SEC("prog")
 int xdp_prog_simple(struct xdp_md *ctx)
 {
+	/* data and data_end are pointers to the beginning and end of the packet’s raw
+    memory. Note that ctx->data and ctx->data_end are of type __u32, so we have
+    to perform the casts */
 	void *data_end = (void *)(uintptr_t)ctx->data_end;
 	void *data = (void *)(uintptr_t)ctx->data;
 	
@@ -47,7 +50,12 @@ int xdp_prog_simple(struct xdp_md *ctx)
 	struct iphdr *iph = (struct iphdr *)(eth + 1);
 	struct icmphdr *icmph = (struct icmphdr *)(iph + 1);
 
-	/* sanity check needed by the eBPF verifier */
+	/* sanity check needed by the eBPF verifier
+    When accessing the data in struct ethhdr, we must make sure we don't
+    access invalid areas by checking whether data + sizeof(struct ethhdr) >
+    data_end, and returning without further action if it's true. This check
+    is compulsory by the BPF verifer that verifies your program at runtime. */
+
 	if (OVER(eth, data_end))
 		return XDP_DROP;
 
@@ -62,11 +70,35 @@ int xdp_prog_simple(struct xdp_md *ctx)
 	if (OVER(icmph, data_end))
 		return XDP_DROP;
 
-    /* let other packets pass */
+	/* 
+	struct iphdr {
+	#if defined(__LITTLE_ENDIAN_BITFIELD)
+		__u8	ihl:4,
+			version:4;
+	#elif defined (__BIG_ENDIAN_BITFIELD)
+		__u8	version:4,
+  			ihl:4;
+	#else
+	#error	"Please fix <asm/byteorder.h>"
+	#endif
+		__u8	tos;
+		__be16	tot_len;
+		__be16	id;
+		__be16	frag_off;
+		__u8	ttl;
+		__u8	protocol;
+		__sum16	check;
+		__be32	saddr;
+		__be32	daddr;     
+	}; 
+	This is the ipheader structure from ip.h; we can see the elements we can access 
+    and their types. We can use iph->protocol to determine whether an incoming 
+    packet is an ICMP packet or not. */
+
 	if (iph->protocol != IPPROTO_ICMP)
 		return XDP_PASS;
 
-	/* drop icmp */
+	/* Check protocol of the packet */
     if (iph->protocol == IPPROTO_ICMP) {
         /* Get source address */
         __be32 source = iph->saddr;
